7f1d49f478aa2cf1b937e1756405691e9563560d
yatw <yatw@uci.edu>
2019-03-28 18:50:42 -0700

    Heroku dependency

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from connect4 import Connect4
+import os
+
 app = Flask(__name__)
 
 game = Connect4()
@@ -43,4 +45,6 @@ def player_move():
     return jsonify(row=row, color = player_color, win=str(game.has_winner), tie=str(game.tie))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
